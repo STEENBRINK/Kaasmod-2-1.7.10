e676922291909c234ae9665144df7180f6281292
STEENBRINK <bram.steenbrink@hotmail.com>
2014-10-22 19:47:08 +0200

    Added the Fluid, not working jet

--- src/main/java/nl/steenbrink/kaasmod/init/ModBlocks.java
+++ src/main/java/nl/steenbrink/kaasmod/init/ModBlocks.java
@@ -2,15 +2,23 @@ package nl.steenbrink.kaasmod.init;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.fluids.BlockFluidClassic;
+import nl.steenbrink.kaasmod.block.BlockBarrel;
+import nl.steenbrink.kaasmod.block.BlockKaasmodFluidBase;
 import nl.steenbrink.kaasmod.block.BlockSaltOre;
 import nl.steenbrink.kaasmod.reference.Names;
 
 public class ModBlocks {
 
     public static final Block blockSaltOre = new BlockSaltOre();
+    public static final Block blockBarrel = new BlockBarrel();
+    public static final BlockFluidClassic saltWater = new BlockKaasmodFluidBase(ModFluids.saltWater, Material.water);
 
     public static void init() {
         GameRegistry.registerBlock(blockSaltOre, Names.Blocks.SALT_ORE);
+        GameRegistry.registerBlock(blockBarrel, Names.Blocks.BARREL);
+        GameRegistry.registerBlock(saltWater, Names.Fluids.SALT_WATER);
     }
 
 }
