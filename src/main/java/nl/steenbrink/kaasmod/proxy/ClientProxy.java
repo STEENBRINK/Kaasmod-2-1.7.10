b4c8eec452d34925c85788b20a50732fdcc4846d
msvisser <info@webmichiel.nl>
2014-10-31 19:37:33 +0100

    Loaded in the model for the StirlingMachine

--- src/main/java/nl/steenbrink/kaasmod/proxy/ClientProxy.java
+++ src/main/java/nl/steenbrink/kaasmod/proxy/ClientProxy.java
@@ -5,13 +5,17 @@ import net.minecraft.item.Item;
 import net.minecraftforge.client.MinecraftForgeClient;
 import nl.steenbrink.kaasmod.client.renderer.item.ItemRendererBarrel;
 import nl.steenbrink.kaasmod.client.renderer.tileentity.TileEntityRendererBarrel;
+import nl.steenbrink.kaasmod.client.renderer.tileentity.TileEntityRendererStirlingMachine;
 import nl.steenbrink.kaasmod.init.ModBlocks;
 import nl.steenbrink.kaasmod.tileentity.TileEntityBarrel;
+import nl.steenbrink.kaasmod.tileentity.TileEntityStirlingMachine;
 
 public class ClientProxy extends CommonProxy {
     @Override
     public void registerTileEntityRenderer() {
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityBarrel.class, new TileEntityRendererBarrel());
         MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ModBlocks.blockBarrel), new ItemRendererBarrel());
+
+        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityStirlingMachine.class, new TileEntityRendererStirlingMachine());
     }
 }
